f2d0589349f894ef002cfe18a7607fab0d6f1d0a
f-towel <miss071125@gmail.com>
2023-03-13 13:43:01 +0900

    fix

--- talkai/voicevox.py
+++ talkai/voicevox.py
@@ -2,39 +2,82 @@ import json
 import requests
 from pathlib import Path
 from urllib.parse import urljoin
+from dataclasses import dataclass, field
+from speaker import ALL_SPEAKERS
 
 
-BASE_URL = 'http://localhost:50021'
+@dataclass
+class Voicevox:
+    speaker_name: str = field(default='ずんだもん')
+    speaker_style: str = field(default='ノーマル')
+    speed_scale: float = field(default=1.0)
+    pitch_scale: float = field(default=0.0)
+    intonation_scale: float = field(default=1.0)
+    volume_scale: float = field(default=1.0)
+    base_url: str = field(default='http://localhost:50021')
 
+    SPEAKER_MAPPING = ALL_SPEAKERS
 
-def gen_query(text: str) -> dict:
-    params = {'text': text, 'speaker': 2}
-    res = requests.post(
-        urljoin(BASE_URL, 'audio_query'),
-        params=params
-    )
-    return res.json()
+    @classmethod
+    @property
+    def all_speakers(cls):
+        all = cls.SPEAKER_MAPPING
+        return [f'{n} {s}' for n in all for s in all[n]]
 
+    @classmethod
+    @property
+    def all_speaker_id(cls):
+        all = cls.SPEAKER_MAPPING
+        return sorted([n[s] for n in all.values() for s in n])
 
-def gen_synthesis(audio_query_response: dict) -> bytes:
-    params = {'speaker': 2}
-    headers = {'content-type': 'application/json'}
-    audio_query_response_json = json.dumps(audio_query_response)
-    res = requests.post(
-        'http://localhost:50021/synthesis',
-        data=audio_query_response_json,
-        params=params,
-        headers=headers
-    )
-    return res.content
+    @property
+    def _speaker_id(self) -> int:
+        try:
+            return self.SPEAKER_MAPPING[self.speaker_name][self.speaker_style]
+        except KeyError:
+            raise self.InvalidSpeakerError
+
+    def _gen_query(self, text) -> dict:
+        params = {'text': text, 'speaker': self._speaker_id}
+        query = requests.post(
+            urljoin(self.base_url, 'audio_query'),
+            params=params
+        ).json()
+        additional_query = {
+            'speedScale': self.speed_scale,
+            'pitchScale': self.pitch_scale,
+        }
+        query.update(additional_query)
+        return query 
+
+    def _gen_voice(self, query: dict) -> bytes:
+        params = {'speaker': self._speaker_id}
+        headers = {'content-type': 'application/json'}
+        query = json.dumps(query)
+        res = requests.post(
+            urljoin(self.base_url, 'synthesis'),
+            data=query,
+            params=params,
+            headers=headers
+        )
+        return res.content
+
+    def speak(self, text) -> bytes:
+        return self._gen_voice(self._gen_query(text))
+
+    class InvalidSpeakerError(Exception):
+        pass
 
 
 def main():
-    query = post_audio_query('今日のご飯はやきそばです。しかし、私は焼きそばが嫌いです。')
-    audio = post_synthesis(query)
+    vv = Voicevox()
+    audio = vv.speak('これはテスト音声です。')
     with open(Path(f'voice.mp3'), "wb") as f:
        f.write(audio)
 
 
 if __name__ == '__main__':
     main()
+
+
+
