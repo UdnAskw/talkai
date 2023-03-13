fbf39d733ab6a1fecd04a5a6684cf014e6b69e56
f-towel <miss071125@gmail.com>
2023-03-13 13:42:51 +0900

    fix

--- talkai/bot.py
+++ talkai/bot.py
@@ -1,24 +1,33 @@
 import os
+import time
+import queue
+import asyncio
+from io import BytesIO
 from pathlib import Path
 from datetime import datetime
 import discord
-
+import whisper
+import chatgpt
+import voicevox
 from config import DISCORD_BOT_TOKEN
 
 
 bot = discord.Bot()
+q = queue.Queue()
+messages = [
+    {"role": "system", "content": "口語で話して"},
+]
+vv = voicevox.Voicevox()
 
 
 @bot.slash_command(description="ボイスチャンネルに参加")
-async def join(
-    ctx,
-):
+async def join(ctx):
     if ctx.author.voice is None:
         embed = discord.Embed(
             title="Error",
             description="あなたがボイスチャンネルに参加していません",
             color=discord.Colour.red()
-            )
+        )
         await ctx.respond(embed=embed)
         return
     try:
@@ -28,14 +37,13 @@ async def join(
             title="Error",
             description="ボイスチャンネルに接続できません",
             color=discord.Colour.red()
-            )
+        )
         await ctx.respond(embed=embed)
     else:
         embed = discord.Embed(
-            title="Success",
             description="ボイスチャンネルに接続しました",
             color=discord.Colour.green()
-            )
+        )
     finally:
         await ctx.respond(embed=embed)
 
@@ -47,64 +55,65 @@ async def leave(ctx):
             title="Error",
             description="ボイスチャンネルに接続していません",
             color=discord.Colour.red()
-            )
+        )
         await ctx.respond(embed=embed)
     else:
         await ctx.guild.voice_client.disconnect()
         embed = discord.Embed(
-            title="Success",
             description="ボイスチャンネルから切断しました",
             color=discord.Colour.green()
-            )
+        )
         await ctx.respond(embed=embed)
 
 
-@bot.slash_command(description="録音を開始")
-async def record(ctx):
-    if ctx.guild.voice_client is None:
-        embed = discord.Embed(
-            title="Error",
-            description="ボイスチャンネルに接続していません",
-            color=discord.Colour.red()
-            )
-        await ctx.respond(embed=embed)
-    else:
-        ctx.voice_client.start_recording(
-            discord.sinks.MP3Sink(),
+@bot.event
+async def on_ready():
+    print('bot started')
+
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    vc = member.guild.voice_client
+    if before.self_mute and not after.self_mute:
+        print(f'{member} started talking')
+        now = datetime.now().strftime('%Y%m%d%H%M%S')
+        audio_file = Path(f'voice_{member.id}_{now}.mp3').resolve()
+        vc.start_recording(
+            discord.sinks.WaveSink(),
             finished_callback,
-            ctx
-            )
-        embed = discord.Embed(
-            title="Success",
-            description="ボイスチャンネルの録音を開始しました",
-            color=discord.Colour.green()
-            )
-        await ctx.respond(embed=embed)
+            vc,
+        )
+        print('start recording')
+    elif after.self_mute and not before.self_mute:
+        print(f'{member} stopped talking')
+        try:
+            vc.stop_recording()
+        except discord.sinks.RecordingException:
+            pass
+        else:
+            print('stop recording')
 
 
-@bot.slash_command(description="録音を停止します。")
-async def record_stop(ctx):
-    if ctx.guild.voice_client is None:
-        embed = discord.Embed(title="Error",
-            description="ボイスチャンネルに接続していません",
-            color=discord.Colour.red()
-            )
-        await ctx.respond(embed=embed)
-    else:
-        ctx.voice_client.stop_recording()
-        embed = discord.Embed(
-            title="Success",
-            description="ボイスチャンネルの録音を停止しました",
-            color=discord.Colour.green()
-            )
-        await ctx.respond(embed=embed)
-
-
-async def finished_callback(sink, ctx):
-    t = datetime.now().strftime('%Y%m%d%H%M%S')
+async def finished_callback(sink, voice_client):
+    global messages
     for user_id, audio in sink.audio_data.items():
-        with open(Path(f'voice_{t}.mp3'), "wb") as f:
-            f.write(audio.file.getbuffer())
+        audio_data = BytesIO(audio.file.getbuffer())
+        audio_data.name = 'voice.wav'
+
+        text = whisper.transcrible(audio_data)
+        print(f'user: {text}')
+
+        messages += [{'role': 'user', "content": text}]
+        res = chatgpt.chat(messages)
+        print(res)
+        messages += [{'role': 'assistant', "content": res}]
+
+        vb_audio = vv.speak(res)
+        a = BytesIO(vb_audio)
+        print(f'TYPE: {type(a)}')
+        voice_client.play(
+            discord.FFmpegPCMAudio(a, pipe=True)
+        )
 
 
 bot.run(DISCORD_BOT_TOKEN)
